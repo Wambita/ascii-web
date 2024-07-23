2272ed22a00f203af94a6a30a0cb4a8503523438
shfana <wambitafana@gmail.com>
2024-07-23 11:16:17 +0300

    Feat: add hnadler for assests directory

--- main.go
+++ main.go
@@ -12,10 +12,11 @@ func main() {
 		return
 	}
 	fs := http.FileServer(http.Dir("static"))
+	assets := http.FileServer(http.Dir("assets"))
 	http.HandleFunc("/", handler.Home)
 	http.HandleFunc("/ascii-art", handler.Art)
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	http.Handle("/assets/", http.StripPrefix("/assets/", nil))
+	http.Handle("/assets/", http.StripPrefix("/assets/", assets))
 	http.HandleFunc("/about", handler.About)
 	http.HandleFunc("/instructions", handler.Instructions)
 	fmt.Println("Server is starting at http://localhost:8080")

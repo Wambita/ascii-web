10a8d68cc9a7d469ecf9276e9e0e6e261912fb0d
cliffootieno <clifford242526@gmail.com>
2024-07-23 06:00:45 +0300

    chore : make handlers package and function

---
+++ handlers/handler.go
@@ -0,0 +1,62 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+
+	functions "server/functions"
+)
+
+type ascii struct {
+	AsciiArt string
+	Error    string
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	home := template.Must(template.ParseFiles("./template/index.html"))
+	w.WriteHeader(http.StatusOK)
+
+	home.Execute(w, nil)
+}
+
+func ErrorPage(w http.ResponseWriter, statusCode int, message string) {
+	tmpl := template.Must(template.ParseFiles("template/error.html"))
+	w.WriteHeader(statusCode)
+	data := ascii{Error: message}
+	tmpl.Execute(w, data)
+}
+
+func Art(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		ErrorPage(w, http.StatusBadRequest, "400 Bad Request")
+		return
+	}
+
+	r.ParseForm()
+	text := r.FormValue("input")
+	banner := r.FormValue("bannerfile")
+	ascii_Art, err := functions.Input(text, banner)
+	fmt.Println(err)
+
+	if err != nil {
+		if err.Error() == "file not found" {
+
+			// w.WriteHeader(http.StatusNotFound)
+			ErrorPage(w, http.StatusNotFound, "404 Not Found")
+			return
+		}
+
+		ErrorPage(w, http.StatusInternalServerError, "500 Internal Server Error")
+		return
+	}
+
+	fmt.Println(ascii_Art)
+	data := ascii{AsciiArt: ascii_Art}
+
+	home := template.Must(template.ParseFiles("template/index.html"))
+	err2 := home.Execute(w, data)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+}

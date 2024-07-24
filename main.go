409d0bf53543c154d1fab83ad224d40d72a677bb
cliffootieno <clifford242526@gmail.com>
2024-07-24 11:14:05 +0300

    fix: functionality to check for non existent endpoints

--- main.go
+++ main.go
@@ -4,8 +4,24 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+
 	handler "server/handlers"
 )
+
+type customHandler struct {
+	http.Handler
+}
+
+func (c *customHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/", "/ascii-art", "/about", "/instructions":
+		c.Handler.ServeHTTP(w, r)
+	case "/static/", "/assets/":
+		c.Handler.ServeHTTP(w, r)
+	default:
+		handler.ErrorPageHandler(w, http.StatusNotFound, "404 not found")
+	}
+}
 
 func main() {
 	if len(os.Args) != 1 {
@@ -13,12 +29,14 @@ func main() {
 	}
 	fs := http.FileServer(http.Dir("static"))
 	assets := http.FileServer(http.Dir("assets"))
-	http.HandleFunc("/", handler.Home)
-	http.HandleFunc("/ascii-art", handler.Art)
+	// mux := http.NewServeMux()
+	http.HandleFunc("/", handler.HomeHandler)
+	http.HandleFunc("/ascii-art", handler.ArtHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.Handle("/assets/", http.StripPrefix("/assets/", assets))
-	http.HandleFunc("/about", handler.About)
-	http.HandleFunc("/instructions", handler.Instructions)
+	http.HandleFunc("/about", handler.AboutHandler)
+	http.HandleFunc("/instructions", handler.InstructionsHandler)
+	// custom := &customHandler{Handler: mux}
 	fmt.Println("Server is starting at http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }

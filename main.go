74114ebffd8d1f56501e8981973002fa70041bec
cliffootieno <clifford242526@gmail.com>
2024-07-24 17:12:39 +0300

    fix : handle routing issues

--- main.go
+++ main.go
@@ -1,42 +1,41 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 
 	handler "server/handlers"
 )
 
-// type customHandler struct {
-// 	http.Handler
-// }
-
-// func (c *customHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/", "/ascii-art", "/about", "/instructions":
-// 		c.Handler.ServeHTTP(w, r)
-// 	case "/static/", "/assets/":
-// 		c.Handler.ServeHTTP(w, r)
-// 	default:
-// 		handler.ErrorPageHandler(w, http.StatusNotFound, "404 not found")
-// 	}
-// }
-
 func main() {
 	if len(os.Args) != 1 {
 		return
 	}
+
 	fs := http.FileServer(http.Dir("static"))
 	assets := http.FileServer(http.Dir("assets"))
 	// mux := http.NewServeMux()
-	http.HandleFunc("/", handler.HomeHandler)
-	http.HandleFunc("/ascii-art", handler.ArtHandler)
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/home", handler.HomeHandler)
 	http.Handle("/assets/", http.StripPrefix("/assets/", assets))
+	http.HandleFunc("/ascii-art", handler.ArtHandler)
 	http.HandleFunc("/about", handler.AboutHandler)
 	http.HandleFunc("/instructions", handler.InstructionsHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Check if the request path matches any of the existing handlers
+		if r.URL.Path == "/" ||
+		r.URL.Path == "/home" ||
+			r.URL.Path == "/ascii-art" ||
+			r.URL.Path == "/about" ||
+			r.URL.Path == "/instructions" {
+			http.NewServeMux().ServeHTTP(w, r)
+		} else {
+			handler.ErrorPageHandler(w, http.StatusNotFound, "404 - Not Found")
+		}
+	})
+
 	// custom := &customHandler{Handler: mux}
-	fmt.Println("Server is starting at http://localhost:8080")
+
 	http.ListenAndServe(":8080", nil)
 }

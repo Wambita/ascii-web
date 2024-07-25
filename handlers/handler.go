a71b1fc48c98ed8ea6165b4e37c0beda88ae2fe2
cliffootieno <clifford242526@gmail.com>
2024-07-25 12:55:39 +0300

    fix : handle case where error page was not being displayed

--- handlers/handler.go
+++ handlers/handler.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"text/template"
+
 	function "server/ascii"
 )
 
@@ -15,7 +16,7 @@ type ascii struct {
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	home, err := template.New("index.html").ParseFiles("./template/index.html")
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		ErrorPageHandler(w, http.StatusInternalServerError, "500 Internal server error")
 		return
 	}
 	w.WriteHeader(http.StatusOK)
@@ -25,8 +26,9 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 
 func AboutHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.New("about.html").ParseFiles("./template/about.html")
+	fmt.Print(err)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		ErrorPageHandler(w, http.StatusInternalServerError, "500 Internal server error")
 		return
 	}
 	w.WriteHeader(http.StatusOK)
@@ -37,7 +39,7 @@ func AboutHandler(w http.ResponseWriter, r *http.Request) {
 func InstructionsHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.New("instructions.html").ParseFiles("./template/instructions.html")
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		ErrorPageHandler(w, http.StatusInternalServerError, "500 Internal server error")
 		return
 	}
 	w.WriteHeader(http.StatusOK)

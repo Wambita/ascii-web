3909d99eb6592cb243d67730819b2bbd9a4833f5
wnjuguna <wm453856@gmail.com>
2024-07-26 14:08:00 +0300

    docs: Add comment on function

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 	handler "server/handlers"
 )
-
+// Runs the server and calls the various webpages.
 func main() {
 	if len(os.Args) != 1 {
 		return
@@ -16,7 +16,7 @@ func main() {
 	fs := http.FileServer(http.Dir("static"))
 	assets := http.FileServer(http.Dir("assets"))
 
-	// http.HandleFunc("/", handler.HomeHandler)
+	
 	http.Handle("/assets/", http.StripPrefix("/assets/", assets))
 	http.HandleFunc("/ascii-art", handler.ArtHandler)
 	http.HandleFunc("/about", handler.AboutHandler)

7ec74d9ee23876bb679ce621dcf190a513d8d287
cliffootieno <clifford242526@gmail.com>
2024-07-27 16:48:22 +0300

    refactor: Add method feature to handle the various methods

--- handlers/handler.go
+++ handlers/handler.go
@@ -11,14 +11,18 @@ import (
 type ascii struct {
 	AsciiArt string
 	Error    string
-	Output string
+	Output   string
 }
 
 // Handler for the homepage.
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Invalid request method", http.StatusBadRequest)
+	}
+
 	home, err := template.New("index.html").ParseFiles("./template/index.html")
 	if err != nil {
-		ErrorPageHandler(w, http.StatusInternalServerError, "500 Internal server error")
+		ErrorPageHandler(w, http.StatusInternalServerError, "400 Bad Request")
 		return
 	}
 	w.WriteHeader(http.StatusOK)
@@ -28,10 +32,14 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 
 // Handler for the about page
 func AboutHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Invalid request method", http.StatusBadRequest)
+	}
+
 	tmpl, err := template.New("about.html").ParseFiles("./template/about.html")
 	fmt.Print(err)
 	if err != nil {
-		ErrorPageHandler(w, http.StatusInternalServerError, "500 Internal server error")
+		ErrorPageHandler(w, http.StatusInternalServerError, "400 Bad Request")
 		return
 	}
 	w.WriteHeader(http.StatusOK)
@@ -41,9 +49,13 @@ func AboutHandler(w http.ResponseWriter, r *http.Request) {
 
 // Handler for the instruction page.
 func InstructionsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Invalid request method", http.StatusBadRequest)
+	}
+
 	tmpl, err := template.New("instructions.html").ParseFiles("./template/instructions.html")
 	if err != nil {
-		ErrorPageHandler(w, http.StatusInternalServerError, "500 Internal server error")
+		ErrorPageHandler(w, http.StatusInternalServerError, "400 Bad Request")
 		return
 	}
 	w.WriteHeader(http.StatusOK)
@@ -63,10 +75,10 @@ func ErrorPageHandler(w http.ResponseWriter, statusCode int, message string) {
 	tmpl.Execute(w, data)
 }
 
-//Handler used for obtaining ascii art
+// Handler used for obtaining ascii art
 func ArtHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		ErrorPageHandler(w, http.StatusBadRequest, "400 Bad Request")
+		ErrorPageHandler(w, http.StatusBadRequest, "400 - Bad Request")
 		return
 	}
 
@@ -77,14 +89,14 @@ func ArtHandler(w http.ResponseWriter, r *http.Request) {
 	ascii_Art, err := function.Input(text, banner)
 
 	if text == "" || banner == "" {
-		ErrorPageHandler(w, http.StatusBadRequest, "400 Bad Request")
+		ErrorPageHandler(w, http.StatusBadRequest, "400- Bad Request")
 		return
 	}
 
 	banners := []string{"standard", "thinkertoy", "shadow"}
 	for i := range banners {
 		if banner != banners[i] && i == len(banners)-1 {
-			ErrorPageHandler(w, http.StatusBadRequest, "400 Bad Request")
+			ErrorPageHandler(w, http.StatusBadRequest, "400 - Bad Request")
 			return
 		} else if banner == banners[i] {
 			break
@@ -93,20 +105,20 @@ func ArtHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		if err.Error() == "file not found" {
 
-			ErrorPageHandler(w, http.StatusNotFound, "404 Not Found")
+			ErrorPageHandler(w, http.StatusNotFound, "404 - Not Found")
 			return
 		}
 
-		ErrorPageHandler(w, http.StatusInternalServerError, "500 Internal Server Error")
+		ErrorPageHandler(w, http.StatusInternalServerError, "400 - Bad Request")
 		return
 	}
 
-	data := ascii{AsciiArt: ascii_Art,Output: text}
+	data := ascii{AsciiArt: ascii_Art, Output: text}
 
 	tmpl := template.Must(template.ParseFiles("template/ascii.html"))
 	err1 := tmpl.Execute(w, data)
 	if err1 != nil {
-		ErrorPageHandler(w, http.StatusBadRequest, "404 Not Found")
+		ErrorPageHandler(w, http.StatusBadRequest, "404 - Not Found")
 		return
 	}
 }
